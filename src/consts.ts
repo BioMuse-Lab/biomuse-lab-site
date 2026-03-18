34a0d4a6067b3dba360e76ec89ec9ba9a6dc114b
BioMuse_lab <rightheretc@gmail.com>
2026-03-18 20:41:30 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "就在此地",
+  EMAIL: "rightheretc@163.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -10,21 +10,21 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "一名没有形状的女子 / Thinking, Writing, Painting.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
+  TITLE: "博文",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
+  TITLE: "工作",
   DESCRIPTION: "Where I have worked and what I have done.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
+  TITLE: "作品",
   DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
 };
 
